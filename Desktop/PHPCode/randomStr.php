c7ea3c281dfd99134199be7082c9e4f690739ef7
Sebastian Sebastine <bruce.cms@gmail.com>
2012-10-20 18:17:13 -0500

    Update Desktop/PHPCode/randomStr.php

--- Desktop/PHPCode/randomStr.php
+++ Desktop/PHPCode/randomStr.php
@@ -2,25 +2,25 @@
 
 function RandomStr($length){  
 
-$arr = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+  $arr = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
 
-$arr = explode(" ", $arr);
+  $arr = explode(" ", $arr);
 
-for ($i=$length; $i>0; $i--)
-  {
-  $x = mt_rand(0, 25); // index of alphabet
-  $y = mt_rand(0, 9); // only use single digit numbers
-  $random = mt_rand(0, 1);
-  $case = mt_rand(0, 1); // additional randomness, incase $random is already set 
+    for ($i=$length; $i>0; $i--)
+     {
+       $x = mt_rand(0, 25);     // index of alphabet
+       $y = mt_rand(0, 9);      // only use single digit numbers
+       $random = mt_rand(0, 1);
+       $case = mt_rand(0, 1);   // additional randomness, incase $random is already set 
   
-  $char = ($random > 0) ? ($charCase = ($case > 0) ? $arr[$x] : strtoupper($arr[$x])) : $y;
+       $char = ($random > 0) ? ($charCase = ($case > 0) ? $arr[$x] : strtoupper($arr[$x])) : $y;
 
-  $str .= "$char"; 
-  }
+       $str .= "$char"; 
+     }
 
-  return ($str); 
+     return ($str); 
 } 
 
-echo RandomStr(8);
+  echo RandomStr(8);
 
 ?>
