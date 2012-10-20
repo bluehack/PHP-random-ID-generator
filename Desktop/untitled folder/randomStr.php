9f1d4cad7434795bb063a4df1b6f6dba626a79ae
computer <computer@cpe-70-113-64-185.austin.res.rr.com>
2012-10-20 00:49:59 -0500

    PHP

---
+++ Desktop/untitled folder/randomStr.php
@@ -0,0 +1,26 @@
+<?php
+
+function RandomStr($length){  
+
+$arr = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+
+$arr = explode(" ", $arr);
+
+for ($i=$length; $i>0; $i--)
+  {
+  $x = rand(0, 25); // index of alphabet
+  $y = rand(0, 9); // only use single digit numbers
+  $random = rand(0, 1);
+  $case = rand(0, 1); // additional randomness, incase $random is already set 
+  
+  $char = ($random > 0) ? ($charCase = ($case > 0) ? $arr[$x] : strtoupper($arr[$x])) : $y;
+
+  $str .= "$char"; 
+  }
+
+  return ($str); 
+} 
+
+echo RandomStr(8);
+
+?>
